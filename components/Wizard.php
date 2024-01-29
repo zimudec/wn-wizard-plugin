c68232df2a0077892d2d5a8de0c690407a4e46ae
Héctor <hectzimudec@gmail.com>
2024-01-29 17:57:09 -0300

    - Adjustments validating previous steps in the final step - When detecting validation problems from previous steps in an ajax request, redirect correctly to step 1 - If the session is lost, it is immediately redirected to step 1, without validating anything - When changing pages and being in step 2 or higher, return to step 1, for safety - If you try to enter a section that does not exist, you will be redirected to step 1 - If incorrect step is loaded, returns last valid step - Calculations made in runtime validation are reused, for performance - Extra data can be saved in session, from anywhere in the system - Each wizard has their own session, to prevent one from stepping on the other - User can see the passage of each car in the page title

--- components/Wizard.php
+++ components/Wizard.php
@@ -44,9 +44,12 @@ class Wizard extends ComponentBase
             $validator->after(function ($validator) use ($validationExtra, $requestData, &$resp) {
                 $result = $validationExtra($validator, $requestData, $resp);
 
-                if (is_array($result)) {
-                    $resp = array_merge($resp, $result);
-                }
+                /* if (is_array($result)) {
+                $resp = array_merge($resp, $result);
+                } */
+
+                $session = session()->get($this->sessionNameGet(), []);
+                $resp = array_merge($resp, $session['validations']);
             });
         }
 
@@ -63,21 +66,56 @@ class Wizard extends ComponentBase
 
     public function sendToStep($step)
     {
-        if (preg_match('/\:step\?/', $this->page->url)) {
-            if (isset($this->steps[$step]['step'])) {
-                redirect(Page::url($this->page->fileName, ['step' => $this->steps[$step]['step']]))->send();
-                exit();
-            } else {
-                dump('You must correctly define the wizard component configuration arrangement');
-            }
-        } else {
+        if (!preg_match('/\:step\?/', $this->page->url)) {
             dump('You must add "/:step?" at the end the url of your page');
+            return;
         }
+
+        if (!isset($this->steps[$step]['step'])) {
+            dump('You must correctly define the wizard component configuration arrangement');
+            return;
+        }
+
+        $destiny = Page::url($this->page->fileName, ['step' => $this->steps[$step]['step']]);
+
+        if (request()->ajax()) {
+            $headers = ['Content-Type' => 'application/json'];
+            response()->json(['X_WINTER_REDIRECT' => $destiny], 200, $headers)->send();
+        } else {
+            redirect($destiny)->send();
+        }
+        exit();
     }
 
     public function sendFirstStep()
     {
         $this->sendToStep(0);
+    }
+
+    public function sessionNameGet()
+    {
+        return 'wizard_steps-' . $this->page->fileName;
+    }
+
+    public function sessionGet()
+    {
+        return session($this->sessionNameGet());
+    }
+
+    public function saveInSession($data)
+    {
+        if (is_array($data)) {
+            $sessionName = $this->sessionNameGet();
+
+            // Guardar datos en sesión
+            $session = session()->get($sessionName, []);
+            if (!isset($session['validations'])) {
+                $session['validations'] = [];
+            }
+            $session['validations'] = array_merge($session['validations'], $data);
+
+            session([$sessionName => $session]);
+        }
     }
 
     public function formsValidate($validatePrevSteps = false)
@@ -86,24 +124,40 @@ class Wizard extends ComponentBase
             exit();
         }
 
+        // Apply current form validation
         if (request()->ajax()) {
-            // Validate if it is allowed to enter the step, according to the data of the previous steps (session)
-            $wizardSession = session()->get('wizard_steps', []);
-            $resp = ($validatePrevSteps
-                ? $this->stepValidate()
-                : (isset($wizardSession['validations']) ? $wizardSession['validations'] : [])
-            );
 
+            $sessionName = $this->sessionNameGet();
+            $wizardSession = session()->get($sessionName, []);
             $stepPos = 0;
+
             foreach ($this->steps as $key => $step) {
                 if ($step['step'] == $this->param('step')) {
+
+                    // Validate if it is allowed to enter the step, according to the session data
+                    $stepValid = 0;
+                    if (count($wizardSession) > 0 && isset($wizardSession['stepCurrent'])) {
+                        $stepValid = $wizardSession['stepCurrent'];
+                    }
+
+                    if ($key > $stepValid) {
+                        $this->sendToStep($stepValid);
+                    }
+
                     $stepPos = $key;
                     break;
                 }
             }
 
+            // Validate current step, and previous ones if necessary
+            $resp = ($validatePrevSteps
+                ? $this->stepValidate()
+                : (isset($wizardSession['validations']) ? $wizardSession['validations'] : []));
+
             $methodFull = explode('::', request()->header('x-winter-request-handler'));
             $method = isset($methodFull[1]) ? $methodFull[1] : $methodFull[0];
+
+            $result = null;
 
             if (
                 isset($this->steps[$stepPos]['forms'][$method]['validation']) ||
@@ -124,10 +178,10 @@ class Wizard extends ComponentBase
                 );
 
                 if (isset($formStep['extra_validation'])) {
-                    $validator->after(function ($validator) use ($formStep, &$resp, $requestData) {
+                    $validator->after(function ($validator) use ($formStep, &$resp, $requestData, $sessionName, &$result) {
                         $result = $formStep['extra_validation'](
                             $validator,
-                            array_merge($requestData, session()->get('wizard_steps', [])),
+                            array_merge(session()->get($sessionName, []), $requestData),
                             $resp
                         );
 
@@ -143,22 +197,21 @@ class Wizard extends ComponentBase
                 }
 
                 // Save data in session
-                $session = array_merge(session()->get('wizard_steps', []), $requestData);
+                $session = array_merge(session()->get($sessionName, []), $requestData);
                 if (!isset($session['validations'])) {
                     $session['validations'] = [];
                 }
                 $session['validations'] = array_merge($session['validations'], $resp);
 
-                session(['wizard_steps' => $session]);
+                session([$sessionName => $session]);
             }
 
             // Save last validated step
-            session([
-                'wizard_steps' => array_merge(session()->get('wizard_steps', []), ['stepCurrent' => ($stepPos + 1)])
-            ]);
+            session([$sessionName => array_merge(session()->get($sessionName, []), ['stepCurrent' => ($stepPos + 1)])]);
 
             // Return next step to redirect
             $resp['stepNext'] = $this->stepNextGet($stepPos);
+            $resp['return'] = $result;
 
             return $resp;
         }
@@ -193,8 +246,9 @@ class Wizard extends ComponentBase
             }
         }
 
+        $sessionName = $this->sessionNameGet();
         $dataExtra = $this->formValidate(
-            session()->get('wizard_steps', []),
+            session()->get($sessionName, []),
             $validationFields,
             $validationMessages,
             $validationExtras
@@ -228,11 +282,12 @@ class Wizard extends ComponentBase
             }
         }
 
+        $sessionName = $this->sessionNameGet();
         $newSession = array_merge(
-            Arr::except(session()->get('wizard_steps', []), $dataToClear),
+            Arr::except(session()->get($sessionName, []), $dataToClear),
             ['stepCurrent' => $currentStepPos]
         );
-        session(['wizard_steps' => $newSession]);
+        session([$sessionName => $newSession]);
     }
 
     public function onRun()
@@ -240,26 +295,33 @@ class Wizard extends ComponentBase
         // If a step is not defined in the url, redirect to step 1
         if (!$this->param('step')) {
             $this->sendFirstStep();
-            return;
         }
 
         // Build url of next and previous buttons
-        $dataExtra = [];
         $wizardData = [];
         $wizardData['stepCurrent'] = $this->param('step');
         $wizardData['stepNext'] = false;
         $wizardData['stepPrev'] = false;
+        $sessionName = $this->sessionNameGet();
+
+        $dataExtra = [];
+        $stepFound = false;
+
+        $wizardSession = session()->get($sessionName, []);
+        $stepValid = 0;
+        if (count($wizardSession) > 0 && isset($wizardSession['stepCurrent'])) {
+            $stepValid = $wizardSession['stepCurrent'];
+        }
 
         foreach ($this->steps as $key => $step) {
             if ($step['step'] == $wizardData['stepCurrent']) {
-                // Validate if it is allowed to enter the step, according to the data of the previous steps
-                $wizardSession = session()->get('wizard_steps', []);
-                // dd($wizardSession);
-                $stepValid = 0;
-                if (count($wizardSession) > 0 && isset($wizardSession['stepCurrent'])) {
-                    $stepValid = $wizardSession['stepCurrent'];
+
+                // If I am on step 1, clean session
+                if ($key == 0) {
+                    session([$sessionName => []]);
                 }
 
+                // Validate if it is allowed to enter the step, according to the session data
                 if ($key > $stepValid) {
                     $this->sendToStep($stepValid);
                 }
@@ -271,8 +333,8 @@ class Wizard extends ComponentBase
                 }
 
                 $dataExtra = $validatePrevSteps
-                    ? $this->stepValidate()
-                    : (isset($wizardSession['validations']) ? $wizardSession['validations'] : []);
+                ? $this->stepValidate()
+                : (isset($wizardSession['validations']) ? $wizardSession['validations'] : []);
 
                 $this->stepPos = $key;
 
@@ -284,22 +346,28 @@ class Wizard extends ComponentBase
                         ['step' => $this->steps[$key - 1]['step']]
                     );
                 }
-                
+
                 $wizardData['stepCurrentName'] = $this->steps[$key]['name'];
+                $stepFound = true;
                 break;
             }
+        }
 
+        if (!$stepFound) {
+            $this->sendToStep($stepValid);
         }
 
         $wizardData['steps'] = $this->steps;
-        $wizardData['fields'] = session()->get('wizard_steps');
+        $wizardData['fields'] = session()->get($sessionName);
         $wizardData['prevValidationsData'] = $dataExtra;
 
         $this->page['wizard'] = $wizardData;
 
+        // If I am in the last step, clear the whole wizard session, to start again
         if (!$wizardData['stepNext']) {
-            // If I am in the last step, clear the whole wizard session, to start again
-            session(['wizard_steps' => []]);
+            if (!isset($step['keep_sesion']) || !$step['keep_sesion']) {
+                session([$sessionName => []]);
+            }
         } else {
             // If not, clean session fields from steps after the current one
             $this->stepSessionClear($this->stepPos);
